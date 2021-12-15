296ff6bf470153abfaff423f89a666552a98eb39
Ricardo Wurmus <rekado@elephly.net>
2021-12-15 11:21:40 +0100

    statistics: Add r-minimal-3.3.3.
    * modules/past/packages/statistics.scm (r-minimal-3.3.3): New variable.

--- modules/past/packages/statistics.scm
+++ modules/past/packages/statistics.scm
@@ -219,3 +219,15 @@
               (sha256
                (base32
                 "0k2i9qdd83g09fcpls2198q4ykxkii5skczb514gnx7mx4hsv56j"))))))
+
+(define-public r-minimal-3.3.3
+  (package (inherit r-minimal-3.3.1)
+    (version "3.3.3")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://cran/src/base/R-"
+                                  (version-prefix version 1) "/R-"
+                                  version ".tar.gz"))
+              (sha256
+               (base32
+                "0v7wpj89b0i3ad3fi1wak5c93hywmbxv8sdnixhq8l17782nidss"))))))
